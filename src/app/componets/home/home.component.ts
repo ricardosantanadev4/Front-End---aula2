1fe1ae85f6f855112919d0b91b13cf4c89ccb24f
ricardosantanadev4 <ricardosantanadev4@gmail.com.br>
2022-05-05 19:14:31 -0300

    Table

--- src/app/componets/home/home.component.ts
+++ src/app/componets/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Card } from 'src/app/models/card';
+import { Movimentacao } from 'src/app/models/movimentacao';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,24 +9,27 @@ import { Card } from 'src/app/models/card';
 export class HomeComponent implements OnInit {
   // cards e a variavel Card[] indica que essa variavel e do tipo array =[] indica que a variavel vai receber um array vazio 
   cards: Card[] = []
+  movimentacoes: Movimentacao[] = []
 
   constructor() { }
 
   // ngOnInit toda ves que iniciar o componet ele vai chamar o que estiver dentro dele
   ngOnInit(): void {
-    this.initCards()
+    this.initCards();
     // exibe a variavel para ver se os objetos foram adicionado no array
-    console.log(this.cards)
+    console.log(this.cards);
+    this.initMovimentacao();
+    console.log(this.movimentacoes);
   }
 
-  initCards(){
+  initCards() {
     // essas linhas cria um novo card
     // cria uma variavel card do tipo Card recebendo um objeto vazio
     let card: Card = {};
     // acessa variaveis da Classe Card e seta os valores tipo string
     card.title = "Saldo";
     card.subTitle = "Saldo em contas";
-    card.color="primary"
+    card.color = "primary"
 
     // essas linhas cria um novo card
     let card1: Card = {};
@@ -33,23 +37,32 @@ export class HomeComponent implements OnInit {
     card1.subTitle = "Saldo em Conta 1"
     card1.color = "success"
 
-    // essas linhas cria um novo card
-    let card2: Card = {}
-    card2.title="Teste";
-    card2.subTitle="Divida de teste";
-    card2.color="danger"
+    let card2: Card = {};
+    card2.title = "Saldo2";
+    card2.subTitle = "Saldo em conta 2";
+    card2.color = "danger";
 
-    // essas linhas cria um novo card
-    let card3:Card ={};
-    card3.title="Teste1";
-    card3.subTitle="Divida de teste";
-    card3.color="info"
     // adiciona os objetos dentro do array
     // push() adiciona apos o ultimo objeto do array
     this.cards.push(card);
     this.cards.push(card1);
     this.cards.push(card2);
-    this.cards.push(card3)
   }
 
+  initMovimentacao() {
+    // realiza o processo enquanto i for menor que 10
+    for (let i = 0; i < 10; i++) {
+      //  criar uma variavel tipo Movimentacao recebendo um objeto vazio
+      let movimentacao: Movimentacao = {};
+      //  com isso e possivel setar os valores concatenados nas variaveis da Classe Movimentecao
+      movimentacao.id = i;
+      movimentacao.tipo = "despesa" + i;
+      movimentacao.nome = "compra" + i;
+      movimentacao.valor = "R$" + i;
+      movimentacao.data = "09/04/2022";
+
+      //  insere o objeto gerado apos o ultimo elemento do array
+      this.movimentacoes.push(movimentacao)
+    }
+  }
 }
---
+++ src/app/models/movimentacao.ts
@@ -0,0 +1,7 @@
+export class Movimentacao{
+    tipo?: string;
+    id?: number;
+    nome?: string;
+    valor?: string;
+    data?: string;
+}
